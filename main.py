e6a83aa43e3eb10df184ce5cbda213bb4bd6533d
theradman221 <theradman221@gmail.com>
2024-12-30 19:04:41 -0700

    Opens a black box and has constants in constants.py, also has working pygame

--- main.py
+++ main.py
@@ -2,12 +2,24 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
+
 
 def main():
+    pygame.init()
     print("Starting asteroids!")
-
-
-
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    # Main loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("#000000")
+        pygame.display.flip()
+        
 
 
 
