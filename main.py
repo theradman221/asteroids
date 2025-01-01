d10d246368a43339f0b4e7d6c3ada567e9d1038d
theradman221 <theradman221@gmail.com>
2025-01-01 13:04:40 -0700

    Refactored main to use groups to update the drawables and updatables before adding shooting or asteroids

--- main.py
+++ main.py
@@ -14,7 +14,12 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-
+    # Create pygame Groups for the updatable and drawable objects
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    # Add all new Players to the updatable and drawable groups and make a new Player
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     # Main loop
@@ -22,10 +27,19 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        # Before drawing anything black out the screen
         screen.fill("#000000")
+
+        # Update all updatables
         dt = clock.tick(60)/ 1000
-        player.update(dt)
-        player.draw(screen)
+        for item in updatable:
+            item.update(dt)
+
+        # Draw all drawables
+        for item in drawable:
+            item.draw(screen)
+
+        # Update the display last
         pygame.display.flip()
 
 
